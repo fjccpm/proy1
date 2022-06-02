a3a1c2bcf132e140838300b1c1fc4059db9a7cfc
fjccpm <fjccpm@gmail.com>
2022-06-01 19:59:25 -0500

    c

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import react_logo from "../images/react-icon-small.png"
 export default function Navbar() {
     return (
         <nav>
-            <img src={react_logo} className="nav--icon" />
+            <img src={react_logo} className="nav--icon" alt=""/>
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
