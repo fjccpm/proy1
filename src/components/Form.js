5cf92f116cf698b3273245c374578717655f6b8f
fjccpm <fjccpm@gmail.com>
2022-06-07 20:48:28 -0500

    c

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,10 +7,12 @@ export default function Form() {
         lastName: "",
         email: "",
         comments: "",
-        isFriendly: false
+        isFriendly: false,
+        employment: "",
+        favColor: ""
     })
 
-    console.log(formData)
+    //console.log(formData)
 
     function handleChange(event)
     {
@@ -61,6 +63,64 @@ export default function Form() {
                     onChange={handleChange}
                 />
                 <label htmlFor="isFriendly">Are you Friendly?</label>
+                <br />
+
+                <fieldset>
+                <legend>Current employment status</legend>
+                
+                <input 
+                    type="radio"
+                    id="unemployed"
+                    name="employment"
+                    value="unemployed"
+                    checked={formData.employment === "unemployed"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="unemployed">Unemployed</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="part-time"
+                    name="employment"
+                    value="part-time"
+                    checked={formData.employment === "part-time"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="part-time">Part-time</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="full-time"
+                    name="employment"
+                    value="full-time"
+                    checked={formData.employment === "full-time"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="full-time">Full-time</label>
+                <br />
+                
+            </fieldset>
+            <br />
+            
+            <label htmlFor="favColor">What is your favorite color?</label>
+            <br />
+            <select 
+                id="favColor"
+                value={formData.favColor}
+                onChange={handleChange}
+                name="favColor"
+            >
+                <option value="">-- Choose --</option>
+                <option value="red">Red</option>
+                <option value="orange">Orange</option>
+                <option value="yellow">Yellow</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+                <option value="indigo">Indigo</option>
+                <option value="violet">Violet</option>
+            </select>
             </form>
     )
 }
